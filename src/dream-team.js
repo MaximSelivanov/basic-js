41ec4489547189d68b96174fc98ccbfe3706d1c1
MaximSelivanov <max_sell@mail.ru>
2021-03-29 16:43:11 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,16 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-/* module.exports =  */function createDreamTeam(members) {
+module.exports = function createDreamTeam(members) {
 
-  let membersArray = members.map(item => item.split(members));
-  return(membersArray)
+  let membersArr;
+  let team = '';
+
+  if (Array.isArray(members)) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === "string") {
+        membersArr = members[i].trim().split('');
+        team += membersArr[0];
+      }
+    }
+    return team.toUpperCase().split('').sort().join('');
+  } 
+  else {
+    return false;
+  }
+
 
   
 
-
-  
 };
-
-let members = ['Matt', 'Ann', 'Dmitry', 'Max'];
-console.log(createDreamTeam(members))
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  
+  if (!Array.isArray(arr)) {
+    throw new Error('Error');
+  }
+  
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i++; break;
+      case '--discard-prev':
+        if (arr[i - 2] !== '--discard-next') result.pop();
+        break;
+      case '--double-next':
+        if (arr[i + 1] !== undefined) result.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        if (arr[i - 1] !== undefined && arr[i - 2] !== '--discard-next') result.push(arr[i - 1]);
+        break;
+      default:
+        result.push(arr[i]);
+        break;
+    }
+  }
+  return result;
+
 };
